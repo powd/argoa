98a2409cd270cf7ee2cb5e6866ce471534f7b3a1
powd <powdgit@gmail.com>
2022-09-12 22:17:34 +0200

    add shared modules generate-id and utils

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['modules/users/jest.config.js', 'modules/threads/jest.config.js'],
+  projects: [
+    'shared/generate-id/jest.config.js',
+    'modules/users/jest.config.js',
+    'modules/threads/jest.config.js',
+  ],
 };
